6d03d66f2a021b69759ab621ee8eded91e3d60da
Thu <thub2012152@student.ctu.edu.vn>
2024-09-22 10:58:36 +0700

    22092024

--- src/service/cloundinary.js
+++ src/service/cloundinary.js
@@ -1,10 +1,9 @@
-const cloundinary = require('cloudinary').v2;
+const cloundinary = require("cloudinary").v2;
 
 cloundinary.config({
-    cloud_name: 'dptlnmlvx',
-    api_key: '798317993943449',
-    api_secret: 'z9lAkYJq8XdjYq_DW5qJsVFnFYM'
-})
-
+  cloud_name: "dy0nlu2ed",
+  api_key: "954974826363758",
+  api_secret: "N0Y055gYgwp4GYpY1SdP1RNH_ds",
+});
 
 module.exports = cloundinary;
